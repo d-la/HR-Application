4230d720c6a75bcddf0a3b59ebb11c3deda5c46f
Daniel La <la.danieln@gmail.com>
2018-04-06 21:36:52 -0400

    Page loader template

---
+++ templates/page-loader.php
@@ -0,0 +1,12 @@
+<?php
+// require_once $_SERVER['DOCUMENT_ROOT'] . '/root.php';
+// require_once __ROOT__ . '/include/header.php';
+?>
+
+<div class="page-loader text-center">
+  <div class="spinner">
+    <p class="text-center">Page Loading...</p>
+    <i class="fa fa-circle-o-notch fa-spin fa-3x fa-fw"></i>
+    <span class="sr-only">Loading...</span>
+  </div>
+</div>
