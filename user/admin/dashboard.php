99d59ee465c84543ee06d4f808e3ce5c636b52cd
Daniel La <la.danieln@gmail.com>
2018-04-18 20:45:04 -0400

    Organization Admin dashboard UI updates

--- user/admin/dashboard.php
+++ user/admin/dashboard.php
@@ -1,5 +1,6 @@
 <?php
 require_once $_SERVER['DOCUMENT_ROOT'] . '/root.php';
+require_once __ROOT__ . '/include/mysqlconn.php';
 require_once __ROOT__ . '/include/sessionstart.php';
 require_once __ROOT__ . '/include/header.php';
 ?>
@@ -7,6 +8,31 @@ require_once __ROOT__ . '/include/header.php';
   <?php
   require_once 'include/sidebar.php';
   require_once __ROOT__ . '/include/navigation.php';
+
+  $organizationUserCount = 0;
+  $organizationUsersHtml = '';
+
+  $usersValidForSchedule = '';
+  $sql = 'CALL spSelectOrganizationUsers(' . $_SESSION['userOrganizationId'] . ');';
+  $rs = $mysqli->query($sql);
+  if ($rs->num_rows > 0){
+    while ($row = $rs->fetch_assoc()){
+      $organizationUsersHtml .= '<tr><td>' . $row['firstname'] . ' ' . $row['lastname'] . '</td><td>' . $row['role'];
+      if (!$row['userid'] == $_SESSION['userId']){
+        $organizationUsersHtml .= '<div class="inline-button"><a href="?userid=' . $row['organizationid'] . '"><i class="fa fa-cogs"></i></a></div>';
+      }
+      $organizationUsersHtml .= '</tr>';
+      $organizationUserCount++;
+
+      if ($row['usertypeid'] == 3){
+        $usersValidForSchedule .= '<option value="' . $row['userid'] . '">' . $row['firstname'] . ' ' . $row['lastname'] . '</option>';
+      }
+    }
+    $rs->close();
+    $mysqli->next_result();
+  } else {
+    $organizationUsersHtml .= '<tr><td colspan="2">No Users saved in system</td>';
+  }
   ?>
   <div id="content" class="content content-mobile">
     <div class="container-fluid">
@@ -16,14 +42,16 @@ require_once __ROOT__ . '/include/header.php';
             <h1>Dashboard</h1>
           </div>
         </div><!-- end .col-md-12 -->
+      </div><!-- end .row -->
+      <div class="row">
         <div class="col-md-3 col-xs-12">
           <div class="widget application-background">
             <div class="stats-icon">
               <i class="fa fa-building-o"></i>
             </div><!-- end .stats-icon -->
             <div class="stats-info">
-              <h4>Total Organizations</h4>
-              <p>12</p>
+              <h4>Total Users</h4>
+              <p><?php echo $organizationUserCount; ?></p>
             </div><!-- end .stats-info -->
             <div class="stats-link">
               <a href="javascript:;">
@@ -84,10 +112,12 @@ require_once __ROOT__ . '/include/header.php';
             </div><!-- end .stats-link -->
           </div><!-- end .widget -->
         </div><!-- end .col-md-3 -->
+      </div><!-- end .row -->
+      <div class="row">
         <div class="col-xs-12 col-md-6">
           <div class="panel panel-default">
             <div class="panel-custom-heading">
-              <?php include '../include/panelbuttons.php'; ?>
+              <?php include __ROOT__ . '/templates/panel-buttons.php'; ?>
               <h4>Data</h4>
             </div>
             <div class="panel-body">
@@ -97,30 +127,77 @@ require_once __ROOT__ . '/include/header.php';
         <div class="col-xs-12 col-md-6">
           <div class="panel panel-default">
             <div class="panel-custom-heading">
-              <?php include '../include/panelbuttons.php'; ?>
+              <?php include __ROOT__ . '/templates/panel-buttons.php'; ?>
               <h4>Data</h4>
             </div>
             <div class="panel-body">
             </div><!-- end .panel-body -->
           </div><!-- end .panel -->
         </div><!-- end .col-md-6 -->
+      </div><!-- end .row -->
+      <div class="row">
         <div class="col-xs-12 col-md-6">
           <div class="panel panel-default">
             <div class="panel-custom-heading">
-              <?php include '../include/panelbuttons.php'; ?>
-              <h4>Data</h4>
+              <?php include __ROOT__ . '/templates/panel-buttons.php'; ?>
+              <h4>All Organization Users</h4>
             </div>
             <div class="panel-body">
+              <div class="table-responsive">
+                <table class="table table-hover">
+                  <thead>
+                    <th>Name</th>
+                    <th>User Role</th>
+                  </thead>
+                  <?php
+                  echo $organizationUsersHtml;
+                  ?>
+                </table>
+              </div>
             </div>
           </div><!-- end .panel-default -->
         </div><!-- end .col-md-6 -->
         <div class="col-xs-12 col-md-6">
           <div class="panel panel-default">
             <div class="panel-custom-heading">
-              <?php include '../include/panelbuttons.php'; ?>
-              <h4>Data</h4>
+              <?php include __ROOT__ . '/templates/panel-buttons.php'; ?>
+              <h4>Assign Schedule</h4>
             </div>
             <div class="panel-body">
+              <form action="" method="post" enctype="multipart/form-data">
+                <div class="form-group">
+                  <div class="input-group">
+                    <label for="assignedUser">User</label>
+                    <select name="assignedUser" class="form-control">
+                      <option value="0">None Selected</option>
+                      <?php echo !empty($usersValidForSchedule) ? $usersValidForSchedule : ''; ?>
+                    </select>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <div class="row m-b-10">
+                    <div class="col-md-6 col-xs-12">
+                      <label for="startDate">Start Date:</label>
+                      <input type="date" name="startDate" class="form-control">
+                    </div>
+                    <div class="col-md-6 col-xs-12">
+                      <label for="endDate">End Date:</label>
+                      <input type="date" name="endDate" class="form-control">
+                    </div>
+                  </div>
+                  <div class="row m-b-10">
+                    <div class="col-md-6 col-xs-12">
+                      <label for="startTime">Start Time:</label>
+                      <input type="time" name="startTime" class="form-control">
+                    </div>
+                    <div class="col-md-6 col-xs-12">
+                      <label for="endTime">End Time:</label>
+                      <input type="time" name="endTime" class="form-control">
+                    </div>
+                  </div>
+                  <button type="submit" name="scheduleSubmit" class="btn btn-default">Submit Schedule</button>
+                </div>
+              </form>
             </div><!-- end .panel-body -->
           </div><!-- end .panel -->
         </div><!-- end .col-md-6 -->
