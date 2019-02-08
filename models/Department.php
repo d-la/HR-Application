4e883d4ea9d39caaed4200bc95ce37c8d33b566d
Daniel La <la.danieln@gmail.com>
2019-02-08 14:51:05 -0500

    Department model filled with methods

--- models/Department.php
+++ models/Department.php
@@ -2,6 +2,7 @@
 namespace HRApplication;
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/include/mysqlconn.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/models/Session.php';
 
 class Department{
 
@@ -13,11 +14,100 @@ class Department{
      * @param string $departmentName
      * @param string $departmentDesc
      * @param int $organizationId
+     * 
+     * @return boolean true if query is successfull, false if not
      */
-    public function insertNewDepartment(){
+    public function insertNewDepartment($departmentName, $departmentDesc, $organizationId){
 
+        if (is_numeric($organizationId)){
+            $mysqli = initializeMysqlConnection();
+
+            $sqlQuery = "CALL spInsertNewOrganization('";
+            $sqlQuery .= addslashes($mysqli->real_escape_string($departmentName)) . "', ";
+            $sqlQuery .= addslashes($mysqli->real_escape_string($departmentDesc)) . "', ";
+            $sqlQuery .= $organizationId . ');';
+
+            $result = $mysqli->query($sqlQuery);
+
+            $session = new HRApplication\Session;
+
+            if ($result === true){
+                $session->setSession('alert', 'success');
+                return true;
+            } else {
+                error_log($mysqli->error);
+                $session->setSession('alert', 'error');
+                return false;
+            }
+        }
+    }
+
+    /**
+     * Update a departments information. Used mainly by organization admins
+     * 
+     * @param int $departmentId;
+     * @param string $departmentName
+     * @param string $departmentDesc
+     * @param int $organizationId: the organization the deparment belongs to
+     * 
+     * 
+     * @return boolean true if succesfull, false if not
+     */
+    public function updateDepartmentInformation($departmentId, $departmentName, $departmentDesc, $organizationId){
+        if ( is_numeric($organizationId) && is_numeric($departmentId) ){
+            $mysqli = initializeMysqlConnection();
+
+            $sqlQuery = "CALL spUpdateDepartmentInformation($departmentId, ";
+            $sqlQuery .=  "'" . addslashes($mysqli->real_escape_string($departmentName)) . "', ";
+            $sqlQuery .=  "'" . addslashes($mysqli->real_escape_string($departmentDesc)) . "', ";
+            $sqlQuery .= $organizationId . ');';
+
+            $result = $mysqli->query($sqlQuery);
+            $session = new HRApplication\Session;
+
+            if ($result === true){
+                $session->setSession('alert', 'success');
+                return true;
+            } else {
+                error_log($mysqli->error);
+                $session->setSession('alert', 'error');
+                return false;
+            }
+        }
     }
     
+    /**
+     * Return all departments. Might be used by super admin but no one else.
+     * 
+     * @return object Mysql object
+     */
+    public function selectAllDepartments(){
+        $mysqli = initializeMysqlConnection();
+
+        $sqlQuery = 'CALL spSelectAllDepartments();';
+        $resultSet = $mysqli->query($sqlQuery);
+
+        return $resultSet;
+    }
+
+    /**
+     * Selects all departments within an organization. Can be used by both admins and super admins
+     * 
+     * @param int $organizationId 
+     * 
+     * @return object mysql object
+     */
+    public function selectAllDepartmentsFromOrganization($organizationId){
+        if (is_numeric($organizationId)){
+            $mysqli = initializeMysqlConnection();
+
+            $sqlQuery = "CALL spSelectAllDepartmentsFromOrganization($organizationId);";
+
+            $resultSet = $mysqli->query($sqlQuery);
+
+            return $resultSet;
+        }
+    }
 }
 
 
